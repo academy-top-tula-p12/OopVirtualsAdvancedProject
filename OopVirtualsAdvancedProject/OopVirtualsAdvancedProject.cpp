67a3ae92b60b78f6b170774d42c42aea01f5f058
maxefimov <efimov.top.academy@gmail.com>
2023-04-25 20:46:57 +0300

    Добавьте файлы проекта.

---
+++ OopVirtualsAdvancedProject/OopVirtualsAdvancedProject.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    virtual ~Base() = default;
+};
+
+class Derived : public Base
+{
+    int* array{};
+public:
+    Derived(int size) : array{ new int[size] } {}
+    ~Derived() override
+    {
+        cout << "Derived destruct\n";
+        delete[] array;
+    }
+};
+
+
+int main()
+{
+    Base* obj = new Base();
+
+    delete obj;
+}
